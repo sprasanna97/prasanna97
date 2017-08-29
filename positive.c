ba8019f5fce33ecac05e541c740af172e8f4cd7a
sprasanna97 <31242014+sprasanna97@users.noreply.github.com>
2017-08-29 16:25:09 +0530

    Rename p.c to positive.c

---
+++ positive.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+#include<conio.h>   
+int main() 
+{  
+int number;  
+printf("Enter a Number\n");  
+scanf("%d", &number);  
+if(number > 0) 
+{  
+printf("%d is Positive Number", number);  
+} 
+else if (number < 0)]
+{  
+printf("%d is Negative Number", number);  
+} 
+else 
+{  
+printf("Input Number is Zero");  
+}  
+return 0;  
+}
+
